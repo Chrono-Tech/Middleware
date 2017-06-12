36361065a8ac04d6d48101a95c4d7cbe1a673dbe
zyevegor <zyev.egor@gmail.com>
2017-06-12 09:20:18 +0300

    https   chronobank atlassian net browse MINT 241   middleware   update event listener

--- tests/core/eventsCore.test.js
+++ tests/core/eventsCore.test.js
@@ -11,8 +11,8 @@ const config = require('../../config.json'),
   chronoBankPlatform_definition = require("../../SmartContracts/build/contracts/ChronoBankPlatform"),
   chronomint_definition = require("../../SmartContracts/build/contracts/LOCManager"),
   mongoose = require('mongoose'),
-  emitters = {},
   contracts = {},
+  contracts_instances = {},
   factory = {};
 
 jasmine.DEFAULT_TIMEOUT_INTERVAL = 30000;
@@ -23,22 +23,20 @@ beforeAll(() => {
   let chronoBankPlatformEmitter = contract(chronoBankPlatformEmitter_definition);
   let chronoMint = contract(chronomint_definition);
 
-
   [chronoBankPlatform, chronoBankPlatformEmitter, chronoMint]
     .forEach(c => {
       c.defaults({from: web3.eth.coinbase, gas: 3000000});
       c.setProvider(provider);
     });
 
-  return new Promise((res) =>
-    helpers.setup.setup(res)
-  )
-    .then(()=>
-    Promise.all([
-      //chronoBankPlatformEmitter.at(helpers.setup.eventsHistory.address),
-      chronoMint.at(helpers.setup.contracts.MultiEventsHistory.address)
-    ])
-    )
+  return helpers.setup()
+    .then((data) => {
+      _.merge(contracts_instances, data.instances);
+      return Promise.all([
+        //chronoBankPlatformEmitter.at(helpers.setup.eventsHistory.address),
+        chronoMint.at(contracts_instances.MultiEventsHistory.address)
+      ])
+    })
     .then((data) => {
       contracts.mint = _.last(data);
       return mongoose.connect(config.mongo.uri);
@@ -74,7 +72,7 @@ test('add new loc', () => {
         currency: helpers.bytes32('LHT')
 
       };
-      return helpers.setup.contracts.LOCManager.addLOC(
+      return contracts_instances.LOCManager.addLOC(
         factory.Loc.name,
         factory.Loc.website,
         factory.Loc.issueLimit,
@@ -95,7 +93,7 @@ test('fetch changes for loc via getLoc', () =>
     contracts.mint.allEvents({fromBlock: 0}).watch((err, result) => {
       if (result && result.event === 'NewLOC') {
         expect(result.args.locName).toBeDefined();
-        helpers.setup.contracts.LOCManager.getLOCByName(result.args.locName)
+        contracts_instances.LOCManager.getLOCByName(result.args.locName)
           .then(data => {
             if (data[4] === factory.Loc.hash) {
               res();
@@ -107,96 +105,96 @@ test('fetch changes for loc via getLoc', () =>
 );
 /*
 
-test('validate hash in mongo', () =>
-  Promise.delay(2000)
-    .then(() =>
-      mongoose.model('NewLOC', new mongoose.Schema({
-          locName: {type: mongoose.Schema.Types.Mixed}
-        }
-      )).findOne({locName: factory.Loc.name})
-    )
-    .then(result => {
-      console.log(result);
-      console.log(factory.Loc)
-      expect(result).toBeDefined();
-      expect(result.locName).toBeDefined();
-      return Promise.resolve();
-    })
-);
-*/
+ test('validate hash in mongo', () =>
+ Promise.delay(2000)
+ .then(() =>
+ mongoose.model('NewLOC', new mongoose.Schema({
+ locName: {type: mongoose.Schema.Types.Mixed}
+ }
+ )).findOne({locName: factory.Loc.name})
+ )
+ .then(result => {
+ console.log(result);
+ console.log(factory.Loc)
+ expect(result).toBeDefined();
+ expect(result.locName).toBeDefined();
+ return Promise.resolve();
+ })
+ );
+ */
 /*
-test('update loc', () => {
+ test('update loc', () => {
 
-  const obj = {
-    Data: new Buffer(helpers.generateRandomString()),
-    Links: []
-  };
-  const ipfs_stack = config.nodes.map(node => ipfsAPI(node));
+ const obj = {
+ Data: new Buffer(helpers.generateRandomString()),
+ Links: []
+ };
+ const ipfs_stack = config.nodes.map(node => ipfsAPI(node));
 
-  Promise.all(
-    _.chain(ipfs_stack)
-      .map(ipfs =>
-        ipfs.object.put(obj)
-      )
-      .value()
-  )
-    .then((data) => {
-      factory.Loc.old_hash = factory.Loc.hash;
-      factory.Loc.hash = helpers.bytes32fromBase58(data[0].toJSON().multihash);
-      return contracts.mint.setLOC(
-        factory.Loc.name,
-        factory.Loc.name,
-        factory.Loc.website,
-        factory.Loc.issueLimit,
-        factory.Loc.hash,
-        factory.Loc.expDate
-      )
-    })
-    .then(result => {
-      expect(result).toBeDefined();
-      expect(result.tx).toBeDefined();
-      return Promise.resolve();
-    })
-});
+ Promise.all(
+ _.chain(ipfs_stack)
+ .map(ipfs =>
+ ipfs.object.put(obj)
+ )
+ .value()
+ )
+ .then((data) => {
+ factory.Loc.old_hash = factory.Loc.hash;
+ factory.Loc.hash = helpers.bytes32fromBase58(data[0].toJSON().multihash);
+ return contracts.mint.setLOC(
+ factory.Loc.name,
+ factory.Loc.name,
+ factory.Loc.website,
+ factory.Loc.issueLimit,
+ factory.Loc.hash,
+ factory.Loc.expDate
+ )
+ })
+ .then(result => {
+ expect(result).toBeDefined();
+ expect(result.tx).toBeDefined();
+ return Promise.resolve();
+ })
+ });
 
-test('fetch changes for loc via getLoc', () =>
-  new Promise(res => {
-    contracts.mint.allEvents({fromBlock: 0}).watch((err, result) => {
-      if (result && result.event === 'UpdLOCValue') {
-        expect(result.args.locName).toBeDefined();
-        contracts.mint.getLOCByName(result.args.locName)
-          .then(data => {
-            if (data[4] === factory.Loc.hash) {
-              res();
-            }
-          })
-      }
-    });
-  })
-);
+ test('fetch changes for loc via getLoc', () =>
+ new Promise(res => {
+ contracts.mint.allEvents({fromBlock: 0}).watch((err, result) => {
+ if (result && result.event === 'UpdLOCValue') {
+ expect(result.args.locName).toBeDefined();
+ contracts.mint.getLOCByName(result.args.locName)
+ .then(data => {
+ if (data[4] === factory.Loc.hash) {
+ res();
+ }
+ })
+ }
+ });
+ })
+ );
 
-test('fetch changes for loc via HashUpdate event', () =>
-  new Promise(res => {
-    contracts.mint.allEvents({fromBlock: 0}).watch((err, result) => {
-      if (result && result.event === 'HashUpdate' && result.args.newHash === factory.Loc.hash) {
-        res();
-      }
-    });
-  })
-);
+ test('fetch changes for loc via HashUpdate event', () =>
+ new Promise(res => {
+ contracts.mint.allEvents({fromBlock: 0}).watch((err, result) => {
+ if (result && result.event === 'HashUpdate' && result.args.newHash === factory.Loc.hash) {
+ res();
+ }
+ });
+ })
+ );
 
-test('validate new hash in mongo', () =>
-  Promise.delay(2000)
-    .then(() =>
-      mongoose.model('hashupdates', new mongoose.Schema({
-          oldHash: {type: mongoose.Schema.Types.Mixed},
-          newHash: {type: mongoose.Schema.Types.Mixed}
-        }
-      )).findOne({oldHash: factory.Loc.old_hash, newHash: factory.Loc.hash})
-    )
-    .then(result => {
-      expect(result).toBeDefined();
-      return Promise.resolve();
-    })
-);
-*/
+ test('validate new hash in mongo', () =>
+ Promise.delay(2000)
+ .then(() =>
+ mongoose.model('hashupdates', new mongoose.Schema({
+ oldHash: {type: mongoose.Schema.Types.Mixed},
+ newHash: {type: mongoose.Schema.Types.Mixed}
+ }
+ )).findOne({oldHash: factory.Loc.old_hash, newHash: factory.Loc.hash})
+ )
+ .then(result => {
+ expect(result).toBeDefined();
+ return Promise.resolve();
+ })
+ );
+ */
--- tests/helpers/setup.js
+++ tests/helpers/setup.js
@@ -4,21 +4,14 @@ const Web3 = require('web3'),
   config = require('../../config.json'),
   provider = new Web3.providers.HttpProvider(config.web3.url),
   path = require('path'),
+  require_all = require('require-all'),
   _ = require('lodash'),
-  Promise = require('bluebird');
+  Promise = require('bluebird'),
+  accounts = [],
+  instances = {},
+  contracts = {};
 
 web3.setProvider(provider);
-
-const contracts = require('require-all')({
-  dirname: path.join(__dirname, '../../SmartContracts/build/contracts'),
-  filter: /(^((ChronoBankPlatformEmitter)|(?!(Emitter|Interface)).)*)\.json$/,
-  resolve: Contract => {
-    let c = contract(Contract);
-    c.defaults({from: web3.eth.coinbase, gas: 3000000});
-    c.setProvider(provider);
-    return c;
-  }
-});
 
 const TIME_SYMBOL = 'TIME';
 const TIME_NAME = 'Time Token';
@@ -28,64 +21,90 @@ const LHT_NAME = 'Labour-hour Token';
 
 const fakeArgs = [0, 0, 0, 0, 0, 0, 0, 0];
 
-let accounts;
-let instances = {};
+module.exports = () => {
 
-let setup = function(callback) {
+  return new Promise(function(resolve, reject) {
+    web3.eth.getAccounts((err, acc) => {
+      console.log(acc);
+      resolve(acc);
+    });
+  })
+    .then(r => {
+      accounts.push(...r);
 
-  Promise.all(
-    _.map(contracts, c =>
-      c.deployed()
-        .then(instance => {
-          return _.set(instances, c.toJSON().contract_name, instance)
-        }).catch(err => {
+      console.log(accounts);
+      return Promise.resolve();
+    })
+    .then(() =>
+      require_all({
+        dirname: path.join(__dirname, '../../SmartContracts/build/contracts'),
+        filter: /(^((ChronoBankPlatformEmitter)|(?!(Emitter|Interface)).)*)\.json$/,
+        resolve: Contract => {
+          let c = contract(Contract);
+          c.defaults({from: accounts[0], gas: 3000000});
+          c.setProvider(provider);
+          return c;
+        }
       })
     )
-  )
-    .then(() => {
-      console.log('link addresses');
+    .then((contracts_set) => {
+      _.merge(contracts, contracts_set);
+      return Promise.all(
+        _.map(contracts, c =>
+          c.deployed()
+            .then(instance => {
+              return _.set(instances, c.toJSON().contract_name, instance)
+            }).catch(err => {
+          })
+        )
+      )
+    })
+    .then(() =>
       Promise.all([
         instances.ContractsManager.init(),
         instances.UserManager.init(contracts.ContractsManager.address),
         instances.PendingManager.init(contracts.ContractsManager.address),
         instances.LOCManager.init(contracts.ContractsManager.address)
       ])
-    }).then(() =>
-    Promise.all([
-      instances.ERC20Manager.init(contracts.ContractsManager.address),
-      instances.ExchangeManager.init(contracts.ContractsManager.address),
-      instances.Rewards.init(contracts.ContractsManager.address, 0),
-      instances.Vote.init(contracts.ContractsManager.address),
-      instances.TimeHolder.init(contracts.ContractsManager.address, contracts.ChronoBankAssetProxy.address),
-      instances.ChronoBankAsset.init(contracts.ChronoBankAssetProxy.address, {from: web3.eth.coinbase}),
-      instances.ChronoBankAssetWithFee.init(contracts.ChronoBankAssetWithFeeProxy.address, {from: web3.eth.coinbase}),
-      instances.ChronoBankAssetProxy.init(contracts.ChronoBankPlatform.address, TIME_SYMBOL, TIME_NAME, {from: web3.eth.coinbase}),
-      instances.ChronoBankAssetWithFeeProxy.init(contracts.ChronoBankPlatform.address, LHT_SYMBOL, LHT_NAME, {from: web3.eth.coinbase}),
-      instances.TimeHolder.addListener(instances.Rewards.address),
-      instances.TimeHolder.addListener(instances.Vote.address),
-      instances.UserManager.setupEventsHistory(instances.MultiEventsHistory.address),
-      instances.MultiEventsHistory.authorize(instances.UserManager.address),
-      instances.PendingManager.setupEventsHistory(instances.MultiEventsHistory.address),
-      instances.MultiEventsHistory.authorize(instances.ChronoBankAsset.address),
-      instances.LOCManager.setupEventsHistory(instances.MultiEventsHistory.address),
-      instances.MultiEventsHistory.authorize(instances.PendingManager.address),
-      instances.ERC20Manager.setupEventsHistory(instances.MultiEventsHistory.address),
-      instances.MultiEventsHistory.authorize(instances.ERC20Manager.address),
-      instances.AssetsManager.setupEventsHistory(instances.MultiEventsHistory.address),
-      instances.MultiEventsHistory.authorize(instances.AssetsManager.address),
-      instances.ExchangeManager.setupEventsHistory(instances.MultiEventsHistory.address),
-      instances.MultiEventsHistory.authorize(instances.ExchangeManager.address),
-      instances.Rewards.setupEventsHistory(instances.MultiEventsHistory.address),
-      instances.MultiEventsHistory.authorize(instances.Rewards.address),
-      instances.Vote.setupEventsHistory(instances.MultiEventsHistory.address),
-      instances.MultiEventsHistory.authorize(instances.Vote.address)
-    ])
-  )
-    .then(() => {
-
-      return Promise.all(
+    )
+    .then(() =>
+      Promise.all([
+        instances.ERC20Manager.init(contracts.ContractsManager.address),
+        instances.ExchangeManager.init(contracts.ContractsManager.address),
+        instances.Rewards.init(contracts.ContractsManager.address, 0),
+        instances.Vote.init(contracts.ContractsManager.address),
+        instances.TimeHolder.init(contracts.ContractsManager.address, contracts.ChronoBankAssetProxy.address),
+        instances.ChronoBankAsset.init(contracts.ChronoBankAssetProxy.address, {from: accounts[0]}),
+        instances.ChronoBankAssetWithFee.init(contracts.ChronoBankAssetWithFeeProxy.address, {from: accounts[0]}),
+        instances.ChronoBankAssetProxy.init(contracts.ChronoBankPlatform.address, TIME_SYMBOL, TIME_NAME, {from: accounts[0]}),
+        instances.ChronoBankAssetWithFeeProxy.init(contracts.ChronoBankPlatform.address, LHT_SYMBOL, LHT_NAME, {from: accounts[0]}),
+        instances.TimeHolder.addListener(instances.Rewards.address),
+        instances.TimeHolder.addListener(instances.Vote.address),
+        instances.UserManager.setupEventsHistory(instances.MultiEventsHistory.address),
+        instances.MultiEventsHistory.authorize(instances.UserManager.address),
+        instances.PendingManager.setupEventsHistory(instances.MultiEventsHistory.address),
+        instances.MultiEventsHistory.authorize(instances.ChronoBankAsset.address),
+        instances.LOCManager.setupEventsHistory(instances.MultiEventsHistory.address),
+        instances.MultiEventsHistory.authorize(instances.PendingManager.address),
+        instances.ERC20Manager.setupEventsHistory(instances.MultiEventsHistory.address),
+        instances.MultiEventsHistory.authorize(instances.ERC20Manager.address),
+        instances.AssetsManager.setupEventsHistory(instances.MultiEventsHistory.address),
+        instances.MultiEventsHistory.authorize(instances.AssetsManager.address),
+        instances.ExchangeManager.setupEventsHistory(instances.MultiEventsHistory.address),
+        instances.MultiEventsHistory.authorize(instances.ExchangeManager.address),
+        instances.Rewards.setupEventsHistory(instances.MultiEventsHistory.address),
+        instances.MultiEventsHistory.authorize(instances.Rewards.address),
+        instances.Vote.setupEventsHistory(instances.MultiEventsHistory.address),
+        instances.MultiEventsHistory.authorize(instances.Vote.address)
+      ])
+    )
+    .then(() =>
+      Promise.all(
         _.chain(instances.ChronoBankPlatformEmitter.abi)
-          .filter({type: 'event'})
+          .filter(event=>
+          event.type === 'event' &&
+          _.has(instances.ChronoBankPlatformEmitter.contract[event.name], 'getData')
+          )
           .map(ev => {
             return instances.EventsHistory.addEmitter(instances.ChronoBankPlatformEmitter.contract[ev.name].getData.apply(this, fakeArgs).slice(0, 10),
               instances.ChronoBankPlatformEmitter.address,
@@ -100,14 +119,12 @@ let setup = function(callback) {
           ])
           .value()
       )
-    })
-    .then(() => {
-      callback();
-    })
-    .catch(e => {
+    )
+    .catch(e => console.error(e => 'eventHistory error', e))
+    .then(() =>
+      Promise.resolve({instances})
+    )
+    .catch(function(e) {
       console.log(e);
     });
 };
-
-module.exports.setup = setup;
-module.exports.contracts = instances;
