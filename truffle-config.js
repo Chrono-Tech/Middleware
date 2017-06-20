6181dd7d1078e98e93bd93d44ce333fa13a6a6fb
zyevegor <zyev.egor@gmail.com>
2017-06-20 11:07:24 +0300

    https://chronobank.atlassian.net/browse/MINT-243 - middleware - add multi network support

--- truffle-config.js
+++ truffle-config.js
@@ -5,20 +5,7 @@ module.exports = {
       port: 8547,
       network_id: '*', // Match any network id
       gas: 4700000
-    },
-    /*    development2: {
-     host: 'localhost',
-     port: 8545,
-     network_id: '*', // Match any network id
-     gas: 4700000,
-     from: '0x64a5d8b41ba9d01d64016164bf5b51b48440d46d'
-     },*/
-    development3: {
-      host: 'localhost',
-      port: 8548,
-      network_id: '*', // Match any network id
-      gas: 4700000
-    },
+    }
   },
   migrations_directory: './migrations'
 };
