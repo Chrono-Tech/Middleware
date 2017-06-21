7663abd1d80539e27922303bf8c191d2ce571c4c
zyevegor <zyev.egor@gmail.com>
2017-06-21 14:18:44 +0300

    https://chronobank.atlassian.net/browse/MINT-258 - middleware - expand event listener

--- index.js
+++ index.js
@@ -3,13 +3,14 @@ const mongoose = require('mongoose'),
   blockModel = require('./models').blockModel,
   contractsCtrl = require('./controllers').contractsCtrl,
   eventsCtrl = require('./controllers').eventsCtrl,
+  aggregateTxsByBlockService = require('./services/aggregateTxsByBlockService'),
   emitter = require('events'),
   _ = require('lodash'),
   plugins = require('./plugins'),
   bunyan = require('bunyan'),
   log = bunyan.createLogger({name: 'app'}),
   Promise = require('bluebird'),
-  utilsSolcCoder = require('web3/lib/solidity/coder.js'),
+  solidityEvent = require('web3/lib/web3/event.js'),
   cluster = require('cluster');
 
 /**
@@ -55,19 +56,82 @@ Promise.all([
   blockModel.findOne({network: network}).sort('-block')
 
 ])
-  .spread((contracts_ctx, block) => {
+  .spread((contracts_ctx, currentBlock) => {
 
     let contracts = contracts_ctx.contracts;
     let contract_instances = contracts_ctx.instances;
-    block = _.chain(block).get('block', 0).add(0).value();
+    currentBlock = _.chain(currentBlock).get('block', 0).add(0).value();
     let event_ctx = eventsCtrl(contract_instances, contracts_ctx.web3);
     let eventModels = event_ctx.eventModels;
-    let eventSignatures = event_ctx.signatures;
     let eventEmitter = new emitter();
 
-    log.info(`search from block:${block} for network:${network}`);
+    log.info(`search from block:${currentBlock} for network:${network}`);
     let chain = Promise.resolve();
+    let step = 50;
 
+    let check = () => {
+      console.log('inside with block: ', currentBlock);
+      return new Promise(res =>
+        contracts_ctx.web3.eth.getBlockNumber((err, result) => res(result))
+      )
+        .then(maxBlock =>
+          currentBlock === maxBlock ? Promise.resolve([]) :
+            Promise.map(new Array(maxBlock < currentBlock + step ? maxBlock - currentBlock : currentBlock + step), () =>
+              aggregateTxsByBlockService(contracts_ctx, contracts_ctx.web3.eth.accounts, contracts_ctx.web3, ++currentBlock)
+            )
+        )
+        .then(data => {
+
+          if(_.isEmpty(data)){
+            chain = chain.delay(2000).then(() => check());
+            return;
+          }
+
+           _.chain(data)
+            .map(d=>d.events)
+            .flattenDeep()
+            .forEach(ev=>{
+              console.log(ev);
+              if(!eventModels[ev.event]){
+                return;
+              }
+
+              console.log(ev)
+
+              let new_event = new eventModels[ev.event](_.merge(ev.args, {network: network}));
+              chain = chain.delay(100).then(() => new_event.save());
+              eventEmitter.emit(ev.event, ev.args);
+            })
+            .value();
+
+           //todo record table with tx
+
+          //add new block to mongo
+          let new_block = new blockModel({block: currentBlock, network: network});
+          chain = chain.delay(100).then(() => new_block.save());
+
+
+          console.log('chunk length:', data.length);
+          //console.log(_.filter(data, item => item.events.length > 0 || item.txs.length > 0));
+          chain = chain.delay(2000).then(() => check());
+        });
+    };
+    chain = chain.then(() => check());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
     //chrono has 2 main addresses, from which we receive events, and for each of them we will setup web3.eth.filter
     [contract_instances.MultiEventsHistory.address, contract_instances.EventsHistory.address].forEach(addr => {
 
@@ -78,29 +142,20 @@ Promise.all([
         }
 
         let signature_definition = eventSignatures[result.topics[0]];
-        let args = _.chain(result.topics.slice(1))
-          .transform((result, arg, i) => {
-
-            //in case, bytes variable has 0x first - then we cut it, beacuse web3 add 0x automatically
-            if (signature_definition.inputs[i].type.match(/^bytes([0-9]{1,})(\[([0-9]*)\])*$/) && arg.indexOf('0x') === 0) {
-              arg = arg.slice(2);
-            }
-
-            result[signature_definition.inputs[i].name] = utilsSolcCoder.decodeParam(signature_definition.inputs[i].type, arg);
-          }, {})
-          .value();
+        let result_decoded = new solidityEvent(null, signature_definition).decode(result);
 
         //add new event to mongo
-        let new_event = new eventModels[signature_definition.name](_.merge(args, {network: network}));
+        let new_event = new eventModels[result_decoded.event](_.merge(result_decoded.args, {network: network}));
         chain = chain.delay(100).then(() => new_event.save());
 
         //add new block to mongo
         let new_block = new blockModel({block: result.blockNumber, network: network});
         chain = chain.delay(100).then(() => new_block.save());
-        eventEmitter.emit(signature_definition.name, args);
+        eventEmitter.emit(signature_definition.name, result_decoded.args);
 
       });
     });
+*/
 
     //register plugins
     _.chain(plugins).values()
