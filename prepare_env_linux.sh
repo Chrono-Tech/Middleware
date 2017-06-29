0fd87806dad6044783a3903dd2010446180d59d4
zyevegor <zyev.egor@gmail.com>
2017-06-29 09:01:50 +0300

    https://chronobank.atlassian.net/browse/MINT-258 - middleware - expand event listener

--- prepare_env_linux.sh
+++ prepare_env_linux.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
 
-rm -rf SmartContracts
-
-echo downloading repo...
-
-git clone -b develop https://github.com/ChronoBank/SmartContracts.git
-(cd SmartContracts  && git checkout b7277a1c2f6cdf8fa4df0282a8026ce402f87777)
-
 echo installing...
-
-(cd SmartContracts && npm install --only=production && cp ../truffle-config.js truffle.js)
 
 for i in "$@"
 do
@@ -25,8 +16,8 @@ done
 
 if [[ -n "$INSTALL" ]]; then
     echo "running deployment task of smart contracts..."
-    (cd SmartContracts && node ./node_modules/truffle/cli migrate && node ./node_modules/truffle/cli exec setup/*)
+    (cd node_modules/chronobank-smart-contracts && node ../truffle/cli migrate && node ./node_modules/truffle/cli exec setup/*)
 else
     echo "compiling smart contracts"
-    (cd SmartContracts && node ./node_modules/truffle/cli compile)
+    (cd node_modules/chronobank-smart-contracts && node ../truffle/cli compile)
 fi
