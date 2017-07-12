ce920c0182b24950abdaaf42c9909f1248c5c316
zyevegor <zyev.egor@gmail.com>
2017-07-12 14:00:04 +0300

    https://chronobank.atlassian.net/browse/MINT-344 - Middleware - expand api

---
+++ factories/index.js
@@ -0,0 +1,10 @@
+const require_all = require('require-all');
+
+/** @factory
+ *  @description search for all controllers and expose them
+ */
+module.exports = require_all({
+  dirname     :  __dirname,
+  filter      :  /(.+Factory)\.js$/,
+  recursive: true
+});
