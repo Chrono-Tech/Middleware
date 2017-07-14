b232e04c5a6ec96a6e308b92aabe29692391d3a8
zyevegor <zyev.egor@gmail.com>
2017-07-14 09:46:41 +0300

    https://chronobank.atlassian.net/browse/MINT-344 - Middleware - expand api

--- prepare_env_linux.sh
+++ prepare_env_linux.sh
@@ -2,6 +2,12 @@
 
 echo installing...
 
+package=$(node -p "require('./node_modules/chronobank-smart-contracts/package.json').dependencies.truffle")
+
+echo installing truffle $package
+
+npm install -g $(echo "truffle@"$package)
+
 for i in "$@"
 do
 case $i in
@@ -16,8 +22,8 @@ done
 
 if [[ -n "$INSTALL" ]]; then
     echo "running deployment task of smart contracts..."
-    (cd node_modules/chronobank-smart-contracts && node ../truffle/build/cli.bundled migrate && node ../truffle/build/cli.bundled exec setup/*)
+    (cd node_modules/chronobank-smart-contracts && truffle migrate && truffle exec setup/*)
 else
     echo "compiling smart contracts"
-    (cd node_modules/chronobank-smart-contracts && node ../truffle/build/cli.bundled compile)
+    (cd node_modules/chronobank-smart-contracts && truffle compile)
 fi
