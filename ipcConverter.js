f4aeeee74a1381e6297cab892cef13b3c3ca7e93
zyevegor <zyev.egor@gmail.com>
2017-07-28 09:27:26 +0300

    https://chronobank.atlassian.net/browse/MINT-358 - Middleware - add event callback api

--- ipcConverter.js
+++ ipcConverter.js
@@ -3,28 +3,37 @@ const net = require('net'),
   bunyan = require('bunyan'),
   fs = require('fs'),
   log = bunyan.createLogger({name: 'ipcConverter'}),
+  _ = require('lodash'),
   request = require('request');
 
-const server = net.createServer(stream => {
+const networks = _.keys(config.web3.networks);
 
-  stream.on('data', c => {
-    request.post(`http://${config.web3.networks.development.host}:${config.web3.networks.development.port}`,
-      {body: c.toString()}, (err, resp, body) => {
-        try {
-          JSON.parse(body);
-          stream.write(body);
-        } catch (e) {
-          log.error(e);
-        }
-      });
+
+networks.forEach(network => {
+
+  const server = net.createServer(stream => {
+
+    stream.on('data', c => {
+      request.post(`http://${config.web3.networks[network].host}:${config.web3.networks[network].port}`,
+        {body: c.toString()}, (err, resp, body) => {
+          try {
+            JSON.parse(body);
+            stream.write(body);
+          } catch (e) {
+            log.error(e);
+          }
+        });
+    });
+
+  });
+
+  if (!/^win/.test(process.platform) && !fs.existsSync(`/tmp/${network}`)) {
+    fs.mkdirSync(`/tmp/${process.env.network}`);
+  }
+
+  server.listen(`${/^win/.test(process.platform) ? '\\\\.\\pipe\\' : '/tmp/'}${network}/geth.ipc`, () => {
+    log.info(`Server: on listening for network - ${network}`);
   });
 
 });
 
-if (!/^win/.test(process.platform) && !fs.existsSync('/tmp/development')){
-  fs.mkdirSync('/tmp/development');
-}
-
-server.listen(`${/^win/.test(process.platform) ? '\\\\.\\pipe\\' : '/tmp/'}development/geth.ipc`, () => {
-  log.info('Server: on listening');
-});
