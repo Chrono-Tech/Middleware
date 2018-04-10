8ca516bfcb3aadffae30cc75d79656b9977fb4bc
zyevegor <zyev.egor@gmail.com>
2018-04-10 11:53:08 +0300

    https://chronobank.atlassian.net/browse/MD-280 - dmt installer

--- utils/getRepos.js
+++ utils/getRepos.js
@@ -1,14 +1,26 @@
-const GitHub = require('github-api'),
-  _ = require('lodash'),
-  Promise = require('bluebird');
+/**
+ * Copyright 2017–2018, LaborX PTY
+ * Licensed under the AGPL Version 3 license.
+ * @author Egor Zuev <zyev.egor@gmail.com>
+ */
+
+const request = require('request-promise'),
+  _ = require('lodash');
 
 module.exports = async () => {
-  let gh = new GitHub({
-    token: process.env.GITHUB_API_KEY
-  });
-  let chrono = Promise.promisifyAll(gh.getOrganization('chronobank'));
 
-  let repos = await chrono.getReposAsync();
+  let repos = await request({
+    uri: 'https://api.github.com/orgs/chronobank/repos',
+    headers: {
+      'User-Agent': 'Request-Promise',
+      'Authorization': process.env.GITHUB_API_KEY ? `token ${process.env.GITHUB_API_KEY}` : null
+    },
+    json: true
+  }).catch((err) => {
+    console.log(`can't obtain repos due the following error: ${err}`);
+    console.log('will try to download the specified repos without validation');
+  });
+
   let predicate = new RegExp(/middleware-((?!service).*)-/);
   repos = _.chain(repos)
     .filter(repo => predicate.test(repo.name))
